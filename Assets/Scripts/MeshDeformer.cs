7cdd501bb2724731d1dad9dfdcc542ea9d08a7c1
emr <renquin@awtce.be>
2018-07-03 13:51:07 +0200

    Up to, and including, section 4 basic deformation.

---
+++ Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+
+
+	Mesh deformingMesh;
+	Vector3[] originalVertices, displacedVertices;
+	Vector3[] vertexVelocities;
+
+	// Use this for initialization
+	void Start () {
+		deformingMesh = GetComponent<MeshFilter>().mesh;
+		originalVertices = deformingMesh.vertices;
+		displacedVertices = new Vector3[originalVertices.Length];
+		for (int i = 0; i < originalVertices.Length; i++) {
+			displacedVertices[i] = originalVertices[i];
+		}
+
+		vertexVelocities = new Vector3[originalVertices.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			UpdateVertex(i);
+		}
+		deformingMesh.vertices = displacedVertices;
+		deformingMesh.RecalculateNormals();
+	}
+
+	void UpdateVertex (int i) {
+		Vector3 velocity = vertexVelocities[i];
+		displacedVertices[i] += velocity * Time.deltaTime;
+	}
+
+	public void AddDeformingForce (Vector3 point, float force) {
+		Debug.DrawLine(Camera.main.transform.position, point);
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			AddForceToVertex(i, point, force);
+		}
+	}
+
+	void AddForceToVertex (int i, Vector3 point, float force) {
+		Vector3 pointToVertex = displacedVertices[i] - point;
+		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+		float velocity = attenuatedForce * Time.deltaTime;
+		vertexVelocities[i] += pointToVertex.normalized * velocity;
+	}
+}
